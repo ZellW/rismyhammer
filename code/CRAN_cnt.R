1b5abe809895472d4d9ea4b9cc5906428fc73d51
ZellW <czweaver@hotmail.com>
2017-03-15 17:45:29 -0400

    Started cnt CRAM RMD

--- code/CRAN_cnt.R
+++ code/CRAN_cnt.R
@@ -68,7 +68,7 @@ readByDate$rawHTML <- as.character(readByDate$rawHTML)
 cntDatesDF <- data.frame(matrix(nrow=0, ncol=1))
 colnames(cntDatesDF) <- c("Date")
 
-#Need the dates for each package.  The we can count how many by date
+#Need the dates for each package.  Then we can count how many by date
 for(x in 1:nrow(readByDate)){
      cntDatesDF[x,1] <- substr(readByDate[x,1],11, 20)
 }
@@ -79,101 +79,14 @@ tail(cntDatesDF,1)
 nrow(cntDatesDF)
 
 
-
-
-
-
-
 #############################
+#Different way to get table - WORKS!
+pkgs <- "https://cran.r-project.org/web/packages/available_packages_by_date.html"
+pkgs_raw <- read_html(pkgs) %>% 
+     html_nodes("table") %>% 
+     .[[1]] %>%
+     html_table()
 
-#-----
-extract_url <- function() {
-     url <- list(archive = "https://cran-archive.r-project.org/bin/windows/contrib/",
-          active = "https://cran.r-project.org/bin/windows/contrib/")
-     
-     get_urls <- function(url) {
-          txt <- readLines(url)
-          idx <- grep("\\d.\\d+/", txt)
-          txt[idx]
-          versions <- gsub(".*?>(\\d.\\d+(/)).*", "\\1", txt[idx])
-          versions
-          paste0(url, versions)
-     }
-     z <- lapply(url, get_urls)
-     unname(unlist(z))
-}
-
-
-
-
-# Given a CRAN URL, extract the number of packages and date
-extract_pkg_info <- function(url) {
-     extract_date <- function(txt, fun = max) {
-          txt <- txt[ - grep("[(STATUS)|(PACKAGES)](.gz)*", txt)]
-          pkgs <- grep(".zip", txt)
-          txt <- txt[pkgs]
-          ptn <- ".*?>(\\d{4}-\\d{2}-\\d{2}).*"
-          idx <- grep(ptn, txt)
-          date <- gsub(ptn, "\\1", txt[idx])
-          date <- as.Date(date, format = "%Y-%m-%d")
-          match.fun(fun)(date)
-     }
-     
-     message(url)
-     txt <- readLines(url)
-     count <- length(grep(".zip", txt))
-     # sum(grepl(".zip", txt))
-     
-     # head(txt)
-     data.frame(
-          version = basename(url),
-          date = extract_date(txt),
-          pkgs = count
-     )
-}
-
-
-# Get the list of CRAN URLs
-CRAN_urls <- extract_url()
-CRAN_urls
-
-# Extract package information
-pkgs <- lapply(CRAN_urls, extract_pkg_info)
-pkgs <- do.call(rbind, pkgs)
-head(pkgs)
-tail(pkgs)
-
-pkgs <- head(pkgs, -2) # Remove r-devel and r-future
-
-# Extract major release information
-major_releases <- pkgs[grep("\\.0", pkgs$version),]
-
-#
-library(ggplot2)
-p <- ggplot(pkgs, aes(x = date, y = pkgs)) +
-     geom_smooth() +
-     geom_point() +
-     geom_rug(colour = "grey50") +
-     geom_vline(data = major_releases,
-                aes(xintercept = as.numeric(date)),
-                colour = "grey80") +
-     geom_text(data = major_releases,
-               aes(label = paste("Version", version), y = 8000),
-               angle = 90,
-               colour = "red",
-               hjust = 1, vjust = -1) +
-     geom_text(data = pkgs,
-               aes(label = version, y = 50),
-               colour = "blue") +
-     theme_minimal(16) +
-     ggtitle("Number of CRAN packages per R version") +
-     xlab(NULL) +
-     ylab(NULL)
-
-print(p)
-
-
-          
 #################################
 
 url <- "https://cran.r-project.org/web/packages/available_packages_by_date.html"
@@ -194,3 +107,5 @@ margins = list(l = 100, r = 100, b = 100, t = 100, pad = 4)
 pkgs %>%
      plot_ly(x=pkgs$Date, y=pkgs$published, name="Published packages") %>%
      layout(title = "CRAN packages published ever since.", margin = margins)
+
+#####################################
